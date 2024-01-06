8ebb13dcd91dc040a53e207fcb6f4aafbf375fef
Arthur FRIN <frin.arthur@gmail.com>
2024-01-07 00:25:08 +0100

    add cat project

---
+++ main.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+const bearerToken = 'live_JHo4ZzZdBmVNfwSKPyO3w5T01L7SDxz28DvClwjsben4R2IVkjTREyuq7xml1Qpc';
+
+const appContainer = document.querySelector('#app');
+let newCatUrl = "https://cdn2.thecatapi.com/images/b7f.jpg";  // Déclarer la variable en dehors des fonctions
+
+// Fonction pour mettre à jour l'image du chat
+async function updateCatImage() {
+  newCatUrl = await getNewCatUrl();  // Mettre à jour la variable
+  if (newCatUrl) {
+    const catImage = document.querySelector('.logo');
+    catImage.src = newCatUrl;
+  }
+}
+
+// Ajout d'un bouton qui déclenche la mise à jour de l'image au clic
+appContainer.innerHTML = 
+`
+  <div class="container">
+    <img src="${newCatUrl}" class="logo" alt="Chat" />
+  </div>
+  <button id="changeCatButton">Changer d'image de chat</button>
+`;
+
+// Écouteur d'événements pour le bouton
+document.querySelector('#changeCatButton').addEventListener('click', updateCatImage);
+
+// Initialisation du compteur
+setupCounter(document.querySelector('#counter'));
+
+// Fonction pour obtenir une nouvelle URL d'image de chat
+async function getNewCatUrl() {
+  try {
+    const response = await axios.get('https://api.thecatapi.com/v1/images/search', {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+      },
+    });
+
+    if (response.status !== 200) {
+      throw new Error(`Erreur lors de la requête. Statut : ${response.status}`);
+    }
+
+    const catUrl = response.data[0].url;
+    return catUrl;
+  } catch (error) {
+    console.error('Erreur lors de la récupération de l\'image du chat :', error.message);
+    return null;
+  }
+}
