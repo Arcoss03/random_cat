e31fe63f1347f32f6ce8ae40aec30733a5ab106d
Arthur FRIN <frin.arthur@gmail.com>
2024-01-15 14:21:36 +0100

    add mobile version

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,10 @@ import ky from "ky";
 
 let catUrls: Ref<string[]> = ref([]);
 let pos: Ref<number> = ref(0);
-const catApiUrl: string = "https://api.thecatapi.com/v1/images/search";
+const catApiUrl: string = "https://api.thecatapi.com/v1/images/search?limit=10";
 
 onMounted(async () => {
-  const { url } = await getCat();
-  catUrls.value = [url];
+  catUrls.value = await getCat();
 });
 
 function download() {
@@ -17,7 +16,11 @@ function download() {
 }
 
 function goNext() {
-  if (pos.value < catUrls.value.length - 1) {
+  if (pos.value === catUrls.value.length - 2) {
+    console.log("change cat");
+    pos.value++;
+    changeCat();
+  } else {
     pos.value++;
   }
 }
@@ -28,112 +31,130 @@ function goBack() {
   }
 }
 
-async function getCat(): Promise<{ url: string }> {
+async function getCat(): Promise<string[]> {
   const response = (await ky.get(catApiUrl).json()) as { url: string }[];
-  return response[0];
+  return response.map((cat) => cat.url);
 }
 
 async function changeCat() {
-  const { url } = await getCat();
-  catUrls.value.push(url);
-  pos.value = catUrls.value.length - 1;
+  catUrls.value = [...catUrls.value, ...await getCat()];
 }
 </script>
 
 <template>
-
   <main class="main">
-        <div class="img-container">
-          <img v-if="catUrls.length > 0" :src="catUrls[pos]" class="logo" alt="Chat" />
-          <span v-else class="loader"></span>
-        </div>
-        <div class="button-container">
-          <button @click="goBack" :class="{ 'disabled': pos === 0 }" class="secondary-button" id="goNextButton">Back</button>
-          <button @click="download" id="download" class="primary-button custom-padding">
-            <IconDownload />
-          </button>
-          <button @click="changeCat" class="primary-button" id="changeCatButton">Change cat image</button>
-          <button @click="goNext" :class="{ 'disabled': pos === catUrls.length - 1 }" class="secondary-button" id="goBackButton">Next</button>
-        </div>
-      </main>
+    <div class="img-container">
+      <img v-if="catUrls.length > 0" :src="catUrls[pos]" class="logo" alt="Chat" />
+      <span v-else class="loader"></span>
+    </div>
+    <div class="button-container">
+      <button @click="goBack" :class="{ 'disabled': pos === 0 }" class="secondary-button" id="goNextButton">Back</button>
+      <button @click="download" id="download" class="primary-button custom-padding">
+        <IconDownload />
+      </button>
+      <button @click="goNext" class="secondary-button" id="goBackButton">Next</button>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
-    .main {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-        .img-container {
-            margin-top: 10px;
-            width: 40vw;
-            height: 40vw;
-            border: solid rgb(24, 24, 24) 3px;
-            border-radius: 20px;
-            overflow: hidden;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: rgb(52, 52, 52);
+  .img-container {
+    margin-top: 10px;
+    width: 40vw;
+    height: 40vw;
+    border: solid rgb(24, 24, 24) 3px;
+    border-radius: 20px;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(52, 52, 52);
 
-            img {
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-            }
-        }
-
-        .button-container {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-top: 20px;
-
-            .custom-padding {
-                padding: 12px 15px 8px;
-            }
-        }
+    @media screen and (max-width: 768px) {
+      position: absolute;
+      width: 100vw;
+      height: 100vh;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      margin: 0;
+      border-radius: 0;
+      border: none;
     }
 
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
 
-    .primary-button {
-        background-color: #ff5100;
-        color: #ffffff;
-        border: none;
-        border-radius: 9999px;
-        padding: 10px 15px;
-        margin: 10px;
-        font-size: 1.5rem;
-        cursor: pointer;
-        
-        &:hover {
-            scale: 1.05;
-        }
+  .button-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    @media screen and (max-width: 768px) {
+      position: absolute;
+      bottom: 30px;
     }
 
-    .secondary-button {
-        background-color: #dbdbdb;
-        font-family: "Bungee Spice", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-        color: #141414;
-        border: 1px solid #ff5100;
-        border-radius: 5px;
-        padding: 10px;
-        margin: 10px;
-        font-size: 1.5rem;
-        cursor: pointer;
-        font-family: "Bungee Spice", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-        &:hover {
-            scale: 1.05;
-        }
+    .custom-padding {
+      padding: 12px 15px 8px;
     }
-    .disabled {
-    opacity: 0.5; 
-    cursor: auto;
+  }
+}
+
+
+.primary-button {
+  background-color: #ff5100;
+  color: #ffffff;
+  border: none;
+  border-radius: 9999px;
+  padding: 10px 15px;
+  margin: 10px;
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  &:hover {
+    scale: 1.05;
+  }
+}
+
+.secondary-button {
+  background-color: #dbdbdb;
+  font-family: "Bungee Spice", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  color: #141414;
+  border: 1px solid #ff5100;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  font-family: "Bungee Spice", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+
+  &:hover {
+    scale: 1.05;
+  }
+}
+
+.disabled {
+  opacity: 0.5;
+  cursor: auto;
+
+  &:hover {
+    scale: 1;
+  }
 }
 
 .loader {
@@ -145,8 +166,9 @@ async function changeCat() {
   border: 3px solid #FF3D00;
   box-sizing: border-box;
   animation: rotation 1s linear infinite;
+
   &::after {
-    content: '';  
+    content: '';
     box-sizing: border-box;
     position: absolute;
     left: 4px;
@@ -156,15 +178,14 @@ async function changeCat() {
     height: 10%;
     border-radius: 50%;
   }
-      
+
   @keyframes rotation {
     0% {
       transform: rotate(0deg);
     }
+
     100% {
       transform: rotate(360deg);
     }
-}
-} 
-
-</style>
+  }
+}</style>
